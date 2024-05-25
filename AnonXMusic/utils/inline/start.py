b52dd5401c4c0c815fcef329788fa3d724bd038c
nikkxd1991 <156008721+nikkxd1991@users.noreply.github.com>
2024-05-25 10:51:11 +0530

    Update start.py
    Signed-off-by: nikkxd1991 <156008721+nikkxd1991@users.noreply.github.com>

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -28,9 +28,9 @@ def private_panel(_):
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+        
+        
+        InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
             
     ]
     return buttons
